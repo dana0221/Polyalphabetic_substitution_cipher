140d228ddbe757abef3b969de06c7d43e30da455
dana0221 <dana8478@naver.com>
2022-03-31 12:14:25 +0900

    다중 문자 치환 암호화 js commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// 불러오기 함수
+function loading(){
+
+}
+
+// 결과보기 함수
+function result(){
+
+}
+
+// 초기화 함수
+function reset(){
+    document.getElementById("encryption_key").value='';
+    document.getElementById("plain_text").value='';
+    document.getElementById("cryptogram").value='';
+    document.getElementById("decrypted_text").value='';
+}

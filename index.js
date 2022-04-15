b3ea70619d2055b2d2fc9dcb1e239b10fdc480dc
dana0221 <dana8478@naver.com>
2022-04-15 12:20:23 +0900

    복호화(result_decrypted) commit

--- index.js
+++ index.js
@@ -5,10 +5,13 @@ $(function() {
     })
 })
 
-// 결과보기
-function result_f(){
+// 암호화
+function result_password(){
     let encryption_key = document.getElementById('encryption_key').value
     let plain_text = document.getElementById('plain_text').value
+
+    encryption_key = encryption_key.toLowerCase()
+    plain_text = plain_text.toLowerCase()
 
     // 암호화를 위한 조건
     if(encryption_key == '')
@@ -47,7 +50,7 @@ function result_f(){
         }
 
         // 평문 공백 제거
-        let x1, x2, y1, y2, password = '', decrypted = ''
+        let x1, x2, y1, y2, password = ''
         plain_text = plain_text.replace(/(\s*)/g, "")
 
 
@@ -82,17 +85,82 @@ function result_f(){
             password += ' '
         }
 
-        // 암호문 출력
-        document.getElementById('cryptogram').value = password
+        document.getElementById('result').value = password
+    }
+}
 
-        // 복호문 출력
-        for(let i = 0; i < plain_text.length; i++){
-            if(i % 2 == 0)
-                decrypted += ' '
+// 복호화
+function result_decrypted(){
+    let encryption_key = document.getElementById('encryption_key').value
+    let plain_text = document.getElementById('plain_text').value
 
-            decrypted += plain_text[i]
+    encryption_key = encryption_key.toLowerCase()
+    plain_text = plain_text.toLowerCase()
+
+    // 암호화를 위한 조건
+    if(encryption_key == '')
+        alert('암호키를 입력하세요')
+
+    if(plain_text == '')
+        alert('평문을 입력하세요')
+
+    // 암호키 중복 제거
+    else if(encryption_key != '' && plain_text != '') {
+        const setEncryption_key = new Set()
+        const setPassword_plate = new Set()
+
+        for(let i = 0; i < encryption_key.length; i++){
+            setEncryption_key.add(encryption_key[i])
+
+            // 암호키 암호판에 삽입
+            setPassword_plate.add(encryption_key[i])
         }
-        document.getElementById('decrypted_text').value = decrypted
+
+        // 암호판 생성
+        const arr = 'abcdefghijklmnopqrstuvwxy'
+
+        for(let i = 0; i < 25 ; i++){
+            setPassword_plate.add(arr[i])
+        }
+
+        // 암호판 출력
+        const password_plate = Array.from(setPassword_plate)
+
+        for(let i = 0; i < 25; i++){
+            if(password_plate[i] == 'q')
+                document.getElementById('p' + (i + 1)).value = password_plate[i] + '/z'
+            else
+                document.getElementById('p' + (i + 1)).value = password_plate[i]
+        }
+
+        // 평문 공백 제거
+        let x1, x2, y1, y2, password = ''
+        plain_text = plain_text.replace(/(\s*)/g, "")
+
+        // 복호문 생성
+        for(let i = 0; i < plain_text.length - 1; i += 2){
+            x1 = password_plate.indexOf(plain_text[i]) / 5 | 0
+            y1 = password_plate.indexOf(plain_text[i]) % 5
+
+            x2 = password_plate.indexOf(plain_text[i + 1]) / 5 | 0
+            y2 = password_plate.indexOf(plain_text[i + 1]) % 5
+
+            // 같은 행
+            if(x1 == x2)
+                password += password_plate[x1 * 5 + (y1 + 1) % 5] + password_plate[x2 * 5 + (y2 + 1) % 5]
+
+            // 같은 열
+            else if(y1 == y2)
+                password += password_plate[((x1 - 1) % 5) * 5 + y1] + password_plate[((x2 - 1) % 5) * 5 + y2]
+
+            // 대각선
+            else
+                password += password_plate[x2 * 5 + y1] + password_plate[x1 * 5 + y2]
+
+            password += ' '
+        }
+
+        document.getElementById('result').value = password
     }
 }
 

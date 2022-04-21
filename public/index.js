3f9b1ef01bba58a128d06013c78d82083dc6f668
dana0221 <dana8478@naver.com>
2022-04-21 11:41:23 +0900

    불러오기 함수 에러(선택한 값으로 안 바뀜) 해결 commit

--- public/index.js
+++ public/index.js
@@ -25,7 +25,6 @@ function result_password(){
     // 암호키 중복 제거
     else if(encryption_key != '' && plain_text != '') {
         const setEncryption_key = new Set()
-        const setPassword_plate = new Set()
 
         for(let i = 0; i < encryption_key.length; i++){
             setEncryption_key.add(encryption_key[i])
@@ -55,16 +54,7 @@ function result_password(){
         let x1, x2, y1, y2, password = ''
         plain_text = plain_text.replace(/(\s*)/g, "")
 
-
-        for(let i = 0; i < plain_text.length - 1; i += 2){
-            if(plain_text[i] === plain_text[i + 1])
-                plain_text = plain_text.slice(0, i + 1) + 'x' + plain_text.slice(i + 1)
-        }
-
-        if(plain_text.length % 2 != 0)
-            plain_text += 'x'
-
-        // 암호문 생성
+        // 복호문 생성
         for(let i = 0; i < plain_text.length - 1; i += 2){
             x1 = password_plate.indexOf(plain_text[i]) / 5 | 0
             y1 = password_plate.indexOf(plain_text[i]) % 5
@@ -169,17 +159,15 @@ function result_decrypted(){
 $(function(){
     $('#save').click(function(){
         // 저장하기 위한 조건
-        if(document.getElementById('encryption_key').value == '')
+        if(document.getElementById('encryption_key').value === '')
             alert('암호키를 입력하세요')
 
-        if(document.getElementById('plain_text').value == '')
+        if(document.getElementById('plain_text').value === '')
             alert('평문을 입력하세요')
 
         else{
             const password = document.getElementById('encryption_key').value
             const password_plate = Array.from(setPassword_plate)
-
-            console.log(password, password_plate)
 
             db.collection('password').doc(password).set({"암호키":password, "암호판":password_plate}).then(() => {
                 location.reload()
@@ -191,22 +179,19 @@ $(function(){
 })
 
 // 선택한 암호키, 암호판 불러오기
-function show_password(){
-    let password = ''
+function show_password(password){
     let password_plate = ''
+    password = password.id
 
-    db.collection('password').get().then((pw)=>{
-        pw.forEach((doc)=> {
-            password_plate = doc.data().암호판;
-            password = doc.data().암호키;
+    db.collection('password').doc(password).get().then((pw)=>{
+        password_plate = pw.data().암호판;
+        document.getElementById('encryption_key').value = password
 
-            password_plate = Array.from(password_plate)
-            document.getElementById('encryption_key').value = password
+        password_plate = Array.from(password_plate)
 
-            for(let i = 0; i < 25; i++){
-                document.getElementById('p' + (i + 1)).value = password_plate[i]
-            }
-        })
+        for(let i = 0; i < 25; i++){
+            document.getElementById('p' + (i + 1)).value = password_plate[i]
+        }
     })
 
     document.getElementById("popup").style.display = "none";
